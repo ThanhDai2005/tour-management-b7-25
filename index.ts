c079b7ed80f2e28dd0530431a7b1e56dc1ee93ce
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-04-04 19:33:53 +0700

    1.4. Kết nối database

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connect from "./config/database";
+
 const app: Express = express();
-const port: Number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set("views", `./views`);
 app.set("view engine", "pug");
@@ -9,6 +13,8 @@ app.get("/tours", (req: Request, res: Response) => {
   res.render("client/pages/tours/index");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+connect().then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 });
