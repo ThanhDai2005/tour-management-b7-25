60e38a7b4c1779a20c1c8af47175960976fad039
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-04-07 01:13:13 +0700

    2.1. Thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,42 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 // End Slider Tour Detail
+
+// Cart
+
+// Nếu chưa có giỏ hàng trong localStorage thì tạo giỏ hàng mới cho người dùng
+const existCart = localStorage.getItem("cart");
+
+if (!existCart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Thêm tour vào cart
+const form = document.querySelector("[form-add-to-cart]");
+
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const quantity = parseInt(e.target[0].value);
+    const tourId = parseInt(form.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExistTour = cart.findIndex((item) => item.tourId == tourId);
+
+      if (indexExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity,
+        });
+      } else {
+        cart[indexExistTour].quantity += quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// End Cart
