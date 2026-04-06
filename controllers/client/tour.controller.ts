2c1f1889ccc800a6636bd926849cbada8c651cd2
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-04-06 18:23:54 +0700

    1.1. Tạo khung giao diện

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -43,3 +43,13 @@ export const index = async (req: Request, res: Response) => {
     tours: newTour,
   });
 };
+
+// [GET] /tours/detail/:slugTour
+export const detail = async (req: Request, res: Response) => {
+  const slugTour = req.params.slugTour;
+
+  res.render("client/pages/tours/detail", {
+    pageTitle: "Chi tiết tour",
+    // detail: detail,
+  });
+};
