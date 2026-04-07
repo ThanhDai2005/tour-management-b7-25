a9f4bd22fdb98a6129d338f9e2fabe26f93bea65
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-04-07 15:14:59 +0700

    3.1. Làm giao diện

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,9 +1,12 @@
 import { Express } from "express";
 import tourRoute from "./tour.route";
 import CategoryRoute from "./category.route";
+import CartRoute from "./cart.route";
 
 export const clientRoutes = (app: Express) => {
   app.use("/tours", tourRoute);
 
   app.use("/categories", CategoryRoute);
+
+  app.use("/cart", CartRoute);
 };
