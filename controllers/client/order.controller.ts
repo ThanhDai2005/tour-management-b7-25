d94000c9040e05db17fe8b86aeb0f8c0946a35c6
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-04-08 22:59:30 +0700

    3.2. Lấy data hiển thị ra giao diện

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -62,7 +62,52 @@ export const order = async (req: Request, res: Response) => {
 
 // [GET] /order/success
 export const success = async (req: Request, res: Response) => {
+  const code = req.query.orderCode;
+
+  const order = (await Order.findOne({
+    raw: true,
+    where: {
+      code: code,
+      deleted: false,
+    },
+  })) as any;
+
+  const orderId = order.id;
+
+  const orderItem = (await OrderItem.findAll({
+    raw: true,
+    where: {
+      orderId: orderId,
+    },
+  })) as any;
+
+  for (const item of orderItem) {
+    const tourInfo = (await Tour.findOne({
+      raw: true,
+      where: {
+        id: item.tourId,
+        deleted: false,
+        status: "active",
+      },
+    })) as any;
+
+    item["title"] = tourInfo["title"];
+    item["image"] = JSON.parse(tourInfo["images"])[0];
+    item["slug"] = tourInfo["slug"];
+    item["price"] = tourInfo["price"] * (1 - item["discount"] / 100);
+    item["total"] =
+      tourInfo["price"] * (1 - item["discount"] / 100) * item.quantity;
+  }
+
+  const totalPrice = orderItem.reduce(
+    (total: any, item: any) => total + item.total,
+    0,
+  );
+
   res.render("client/pages/order/success", {
     pageTitle: "Đặt hàng thành công",
+    order: order,
+    orderItem: orderItem,
+    totalPrice: totalPrice,
   });
 };
