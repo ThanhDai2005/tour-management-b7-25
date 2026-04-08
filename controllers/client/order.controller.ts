6f333fca78a225f0adbf6cd1c4e6b87a6b484c68
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-04-08 17:42:45 +0700

    2.3. Server lấy data lưu vào bảng orders_item

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Order from "../../models/order.model";
 import { generateOrderCode } from "../../helpers/generate";
+import OrderItem from "../../models/order-item.model";
+import Tour from "../../models/tour.model";
 
 // [POST] /order
 export const order = async (req: Request, res: Response) => {
@@ -28,6 +30,29 @@ export const order = async (req: Request, res: Response) => {
       },
     },
   );
+
+  for (const item of data.cart) {
+    const dataItem = {
+      tourId: item.tourId,
+      orderId: orderId,
+      quantity: item.quantity,
+    } as any;
+
+    const tourInfo = (await Tour.findOne({
+      raw: true,
+      where: {
+        id: item.tourId,
+        status: "active",
+        deleted: false,
+      },
+    })) as any;
+
+    dataItem["price"] = tourInfo["price"];
+    dataItem["discount"] = tourInfo["discount"];
+    dataItem["timeStart"] = tourInfo["timeStart"];
+
+    await OrderItem.create(dataItem);
+  }
 
   res.status(200).json({
     message: "Đặt hàng thành công!",
